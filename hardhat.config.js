e006680daca8f20605c138d6486c57156f862247
richardngolotus <richard.ngo@lotussoft.vn>
2022-09-16 09:35:25 +0700

    Add config for polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -200,6 +200,14 @@ module.exports = {
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200000000, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    polygon: {
+      url: `https://polygon-rpc.com`,
+      accounts: [`0x${MAINNET_KEY}`],
+      network_id: 137, // Mumbai's id
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200000000, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     }
   },
   paths: {
